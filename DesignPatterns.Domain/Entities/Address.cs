a68766ff7e25d66b8502a0d09e34307fbf99bb2a
VictorSamp <victor.msampaio@outlook.com.br>
2023-07-18 20:08:32 -0300

    feat: add domain project

---
+++ DesignPatterns.Domain/Entities/Address.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Domain.Entities
+{
+    internal class Address
+    {
+        public Guid Id { get; init; }
+        public string Street { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string Number { get; private set; }
+        public string City { get; private set; }
+        public string State { get; set; }
+
+        public Address(string street, string neighborhood, string number,
+            string city, string state)
+        {
+            Street = street;
+            Neighborhood = neighborhood;
+            Number = number;
+            City = city;
+            State = state;
+        }
+    }
+}
---
+++ DesignPatterns.Domain/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Domain.Entities
+{
+    internal class Person
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public Address Address { get; private set; }
+
+        public Person(string name, string lastName, DateTime birthDate, Address address)
+        {
+            Name = name;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Address = address;
+        }
+    }
+}
