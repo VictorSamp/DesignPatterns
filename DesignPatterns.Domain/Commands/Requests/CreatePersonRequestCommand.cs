3b1ed283677ecf628b3ff34d953ebe7e8f6aed1b
VictorSamp <victor.msampaio@outlook.com.br>
2023-07-24 20:08:30 -0300

    feat: add create person request and response commands

---
+++ DesignPatterns.Domain/Commands/Requests/CreatePersonRequestCommand.cs
@@ -0,0 +1,14 @@
+﻿using DesignPatterns.Domain.Entities;
+
+namespace DesignPatterns.Domain.Commands.Requests
+{
+    public class CreatePersonRequestCommand
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public Address Address { get; private set; }
+    }
+}
---
+++ DesignPatterns.Domain/Commands/Responses/CreatePersonResponseCommand.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.Domain.Entities;
+
+namespace DesignPatterns.Domain.Commands.Responses
+{
+    public class CreatePersonResponseCommand
+    {
+        public Guid Id { get; init; }
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public Address Address { get; private set; }
+        public DateTime CreateDate { get; private set; }
+    }
+}
